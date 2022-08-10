cc41e87901e3dfa3a9c9abb36323a9f3442733ed
Ajao Malik <ajaomalik2018@gmail.com>
2022-08-10 12:15:24 -0700

    exec_line.c

--- exec_line.c
+++ exec_line.c
@@ -3,7 +3,7 @@
 /**
  * exec_line - finds builtins and commands
  *
- * @datash: data relevant (args)
+ * @datash:  data relevant (args)
  * Return: 1 on success.
  */
 int exec_line(data_shell *datash)
