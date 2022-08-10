5fdeffc9e43ec17f151a22a37127e06342ede827
Ajao Malik <ajaomalik2018@gmail.com>
2022-08-10 12:12:26 -0700

    cmd_exec.c

--- cmd_exec.c
+++ cmd_exec.c
@@ -2,7 +2,7 @@
 
 /**
  * is_cdir - checks ":" if is in the current directory.
- * @path: type char pointer char.
+ * @path: type  char pointer char.
  * @i: type int pointer of index.
  * Return: 1 if the path is searchable in the cd, 0 otherwise.
  */
