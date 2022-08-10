140134a19ae4cfdcedf2910ae8eef3c8df4a626d
Ajao Malik <ajaomalik2018@gmail.com>
2022-08-10 12:22:31 -0700

    read_line.c

--- read_line.c
+++ read_line.c
@@ -3,7 +3,7 @@
 /**
  * read_line - reads the input string.
  *
- * @i_eof: return value of getline function
+ * @i_eof:  return value of getline function
  * Return: input string
  */
 char *read_line(int *i_eof)
